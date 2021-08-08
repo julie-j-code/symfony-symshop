21cba022e50d7adda868f8c736739f335c06eecf
Julie J <jeannet.julie@gmail.com>
2021-08-08 21:41:25 +0200

    premiers tests

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController{
+
+    public function index(){
+        var_dump("salut") ;
+        die;
+    }
+
+    public function test(Request $request){
+        // dd("test") ;
+
+        // $request=Request::createFromGlobals();
+        dd($request);
+        $age=$request->query->get("age", 0);
+        // si on utilise Request, on retourne forcément une response
+        return new Response("vous avez " .$age. " ans");
+    }
+
+}
+
+?>
