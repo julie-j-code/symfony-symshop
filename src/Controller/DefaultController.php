84d024c70a299721d2c775577a9d6ac5f965e1a6
Julie J <jeannet.julie@gmail.com>
2021-08-09 07:45:41 +0200

    installation de doctrine et création de la base

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,23 +4,28 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController{
 
 /**
- *@Route()
+ *@Route("/", name="index")
  */
     public function index(){
         var_dump("salut") ;
         die;
     }
 
-    public function test(Request $request){
+/**
+* @Route("/test/{age<\d+>?0}", name="test", methods={"GET","POST"},schemes={"http", "https"})
+*/
+
+    public function test(Request $request, $age){
         // dd("test") ;
 
         // $request=Request::createFromGlobals();
-        dd($request);
-        $age=$request->query->get("age", 0);
+        // dd($request);
+        // $age=$request->query->get("age", 0);
         // si on utilise Request, on retourne forcément une response
         return new Response("vous avez " .$age. " ans");
     }
