76427ac1c68728ed1a304cac0aab2ac3338231f0
Julie J <jeannet.julie@gmail.com>
2021-08-11 20:11:46 +0200

    validation de Category par annotations et tests

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert; 
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
@@ -21,6 +22,11 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le nom de la catégorie est obligatoire.")
+     * @Assert\Length(
+     *  min=3,
+     *  minMessage="Le nom doit avoir au moins {{ limit }} caractères."
+     * )
      */
     private $name;
 
@@ -49,7 +55,7 @@ class Category
         return $this->name;
     }
 
-    public function setName(string $name): self
+    public function setName(?string $name): self
     {
         $this->name = $name;
 
