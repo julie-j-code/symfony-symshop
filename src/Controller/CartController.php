468791cf315ecf71ab5519f218e3b2e803e73ee2
Julie J <jeannet.julie@gmail.com>
2021-08-12 19:05:19 +0200

    se faire livrer la SessionInterface pour se passer de la Request

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CartController extends AbstractController
@@ -14,9 +15,8 @@ class CartController extends AbstractController
     /**
      * @Route("/cart/{id}", name="cart_add", requirements={"id":"\d+"})
      */
-    public function index($id, Request $request, ProductRepository $repo): Response
+    public function index($id, ProductRepository $repo, SessionInterface $session): Response
     {
-        // dd($id);
         // 0. S'assurer que le  produit existe
         $product = $repo->find($id);
         if (!$product) {
@@ -25,7 +25,10 @@ class CartController extends AbstractController
 
         // 1. je vérifie si j'ai un panier dans la session
         // 2. si j'ai rien, je passe un tableau vide
-        $cart = $request->getSession()->get('cart', []);
+        // $cart = $request->getSession()->get('cart', []);
+        // ou si je me fais livrer la SessionInterface :
+        $cart = $session->get('cart');
+        // je n'ai plus besoin de la Request  !
         // 3. si le produit $id existe déjà dans le tableau la session
         // 4. je l'incrémante sachant que le tableau ressemblera à
         // ["produit1"=>2, "produit2"=>5]
@@ -36,8 +39,8 @@ class CartController extends AbstractController
             $cart[$id] = 1;
         }
         // 6. Enregistrer le tableau mis à jour dans la session
-        $request->getSession()->set('cart', $cart);
-        ($request->getSession());
+        $session->set('cart', $cart);
+        // dd($session->get('cart'));
         return $this->redirectToRoute("product_show", [
             "category_slug" => $product->getCategory()->getSlug(),
             "slug" => $product->getSlug()
