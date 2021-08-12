d147b9b7ed9919173d2a0067b6fdface63e9ed1e
Julie J <jeannet.julie@gmail.com>
2021-08-12 20:50:08 +0200

    Test FlashBag et affichage des messages Flash dans Twig

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CartController extends AbstractController
@@ -41,6 +42,12 @@ class CartController extends AbstractController
         // 6. Enregistrer le tableau mis à jour dans la session
         $session->set('cart', $cart);
         // dd($session->get('cart'));
+
+        /** @var FlashBag */
+        // tests du FlashBag
+        // $flashBag = $session->get('flashes');
+        $this->addFlash('success', "Le produit a bien été ajouté au panier.");     
+       
         return $this->redirectToRoute("product_show", [
             "category_slug" => $product->getCategory()->getSlug(),
             "slug" => $product->getSlug()
