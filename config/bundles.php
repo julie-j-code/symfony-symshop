de279f878a9a71ba6c2e04edcce5cc7536aa833a
Julie J <jeannet.julie@gmail.com>
2021-08-18 23:14:14 +0200

    Configuration Monolog pour voir les logs de l'application en prod

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
